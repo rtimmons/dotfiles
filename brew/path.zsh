fbee2aa870b9599cdc2caf543c76481537f0e47e
Ryan Timmons <ryan@ganon.com>
2012-07-15 16:23:13 -0400

    make brew installation/update more automated

--- brew/path.zsh
+++ brew/path.zsh
@@ -3,6 +3,8 @@ add_to_path /usr/local/bin
 add_to_path /usr/local/sbin
 
 # Homebrew seems to like to put completions here:
-fpath=($fpath /usr/local/share/zsh/site-functions)
-autoload -U /usr/local/share/zsh/site-functions/*(:t)
+if [ -d "/usr/local/share/zsh/site-functions" ]; then
+    fpath=($fpath /usr/local/share/zsh/site-functions)
+    autoload -U /usr/local/share/zsh/site-functions/*(:t)
+fi
 
