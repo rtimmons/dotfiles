be9c1b29baef6eeb511eb573d881f93d2da01c83
Ryan Timmons <ryan@ganon.com>
2012-07-16 09:25:14 -0400

    ignore hateful/stupid .DS_Store files Make Groovy default to 2.0

--- groovy/env.zsh
+++ groovy/env.zsh
@@ -1,13 +1,16 @@
 
-# Set groovy home if not already set and if 1.8.6 installed 
+# Set groovy home if not already set and if installed 
 # via homebrew
+
+
+[ -z "$GROOVY_HOME" ] \
+    && [ -d "/usr/local/Cellar/groovy/2.0.0/libexec" ] \
+    && export GROOVY_HOME=/usr/local/Cellar/groovy/2.0.0/libexec
+
+# Or try 1.8.6
 
 [ -z "$GROOVY_HOME" ] \
     && [ -d "/usr/local/Cellar/groovy/1.8.6/libexec" ] \
     && export GROOVY_HOME=/usr/local/Cellar/groovy/1.8.6/libexec
 
-# Or try 2.0
 
-[ -z "$GROOVY_HOME" ] \
-    && [ -d "/usr/local/Cellar/groovy/2.0.0/libexec" ] \
-    && export GROOVY_HOME=/usr/local/Cellar/groovy/2.0.0/libexec
