3c4fd7166b0772affb3d05cbee5749c64fbe7830
Ryan Timmons <ryan@ganon.com>
2012-07-20 16:31:21 -0400

    add eclipse oblivion color theme add comment to terminal.app-speedup.sh indicating what it does & give source URL

--- osx/terminal.app-speedup.sh
+++ osx/terminal.app-speedup.sh
@@ -5,5 +5,8 @@
 
 # http://osxdaily.com/2010/05/06/speed-up-a-slow-terminal-by-clearing-log-files/
 
+# This removes all the console entries
+# https://discussions.apple.com/thread/2087220?start=0&tstart=0
+
 sudo rm -rf /private/var/log/asl/*.asl
 
