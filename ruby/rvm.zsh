05e94971764818f39cffad879925c7074886cc00
Ryan Timmons <ryan@ganon.com>
2012-10-08 18:02:48 -0400

    use rbenv instead of rvm

--- ruby/rvm.zsh
+++ ruby/rvm.zsh
@@ -1,7 +1,8 @@
 # are vee em
 
-add_to_path "$HOME/.rvm/bin"
-
-# Load RVM into a shell session *as a function*
-[[ -s "$HOME/.rvm/scripts/rvm" ]] \
-    && source "$HOME/.rvm/scripts/rvm"
+# add_to_path "$HOME/.rvm/bin"
+# 
+# # Load RVM into a shell session *as a function*
+# [[ -s "$HOME/.rvm/scripts/rvm" ]] \
+#     && source "$HOME/.rvm/scripts/rvm"
+# 
