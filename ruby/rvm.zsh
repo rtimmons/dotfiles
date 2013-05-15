7689ff8e56503d8a6d373f3eb22664e894beccc9
Ryan Timmons <ryan@ganon.com>
2013-05-15 18:28:06 -0400

    get rid of references to rvm

--- ruby/rvm.zsh
+++
@@ -1,8 +0,0 @@
-# are vee em
-
-# add_to_path "$HOME/.rvm/bin"
-# 
-# # Load RVM into a shell session *as a function*
-# [[ -s "$HOME/.rvm/scripts/rvm" ]] \
-#     && source "$HOME/.rvm/scripts/rvm"
-# 
