16bf450d75fe7c422821f8e3c9be9fc6e02317e8
Ryan Timmons <ryan@ganon.com>
2013-07-01 23:10:43 -0400

    try a new path for GROOVY_HOME (/usr/local/opt/groovy/libexec)

--- groovy/env.zsh
+++ groovy/env.zsh
@@ -3,6 +3,13 @@
 # via homebrew
 
 [ -z "$GROOVY_HOME" ] \
+    && [ -d "/usr/local/opt/groovy/libexec" ] \
+    && export GROOVY_HOME=/usr/local/opt/groovy/libexec
+
+# Not sure when homebrew groovy started to use that path, so fall back to lots
+# and lots of alternates because I don't care enough to actually research this.
+
+[ -z "$GROOVY_HOME" ] \
     && [ -d "/usr/local/Cellar/groovy/2.1.1/libexec" ] \
     && export GROOVY_HOME=/usr/local/Cellar/groovy/2.1.1/libexec
 
