e84e1ad650d3e294b6eb5c93541e2c867890108d
Ryan Timmons <rtimmons@amazon.com>
2013-11-12 16:44:46 -0500

    add cloud9 stuff

---
+++ cloud9/cloud9.zsh
@@ -0,0 +1,4 @@
+
+export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home'
+export I_WANT_NO_DEVTOOLS_SUPPORT_NOW_AND_FOREVER=JAVA_HOME
+add_to_path $HOME/Cloud9BrazilBuild-1.0/bin
