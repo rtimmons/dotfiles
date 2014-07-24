dac502f9f053665b6b134ac28b9e14b0b8662249
Ryan Timmons <rtimmons@amazon.com>
2014-07-24 10:14:00 -0400

    Cleanup installed brews

--- brew/autojump.zsh
+++ brew/autojump.zsh
@@ -1,7 +1,5 @@
 
 # Init autojump if installed via homebrew
 if (( $+commands[brew] )); then
-    if [ -f `brew --prefix`/etc/autojump ]; then
-      . `brew --prefix`/etc/autojump
-    fi
+    [[ -s `brew --prefix`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh
 fi
