13ba9d6df61b0786362b42dadc7156a009584875
Ryan Timmons <rtimmons@amazon.com>
2014-08-04 10:07:18 -0400

    remove cloud9 stuff

--- cloud9/cloud9.zsh
+++
@@ -1,4 +0,0 @@
-
-export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home'
-export I_WANT_NO_DEVTOOLS_SUPPORT_NOW_AND_FOREVER=JAVA_HOME
-add_to_path $HOME/Cloud9BrazilBuild-1.0/bin
