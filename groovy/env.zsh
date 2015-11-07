a528c3a57f02a989c7cd743010b672269a2cdd1a
Ryan Timmons <rtimmons@amazon.com>
2015-11-07 14:20:19 -0500

    Cleanup GROOVY_HOME management

--- groovy/env.zsh
+++ groovy/env.zsh
@@ -6,35 +6,13 @@
     && [ -d "/usr/local/opt/groovy/libexec" ] \
     && export GROOVY_HOME=/usr/local/opt/groovy/libexec
 
-# Not sure when homebrew groovy started to use that path, so fall back to lots
-# and lots of alternates because I don't care enough to actually research this.
-
-[ -z "$GROOVY_HOME" ] \
-    && [ -d "/usr/local/Cellar/groovy/2.1.1/libexec" ] \
-    && export GROOVY_HOME=/usr/local/Cellar/groovy/2.1.1/libexec
-
-# Or try 2.1.0
-
-[ -z "$GROOVY_HOME" ] \
-    && [ -d "/usr/local/Cellar/groovy/2.1.0/libexec" ] \
-    && export GROOVY_HOME=/usr/local/Cellar/groovy/2.1.0/libexec
-
-# Or try 2.0.0
-
-[ -z "$GROOVY_HOME" ] \
-    && [ -d "/usr/local/Cellar/groovy/2.0.1/libexec" ] \
-    && export GROOVY_HOME=/usr/local/Cellar/groovy/2.0.1/libexec
-
-# Or try 2.0.0
-
-[ -z "$GROOVY_HOME" ] \
-    && [ -d "/usr/local/Cellar/groovy/2.0.0/libexec" ] \
-    && export GROOVY_HOME=/usr/local/Cellar/groovy/2.0.0/libexec
-
-# Or try 1.8.6
-
-[ -z "$GROOVY_HOME" ] \
-    && [ -d "/usr/local/Cellar/groovy/1.8.6/libexec" ] \
-    && export GROOVY_HOME=/usr/local/Cellar/groovy/1.8.6/libexec
+# Support alternative homebrew prefix
+if [ -z "$GROOVY_HOME" ]; then
+    if which brew > /dev/null; then
+        if [ -d "$(brew --prefix)"/opt/groovy/libexec ]; then
+            export GROOVY_HOME="$(brew --prefix)"/opt/groovy/libexec
+        fi
+    fi
+fi
 
 
