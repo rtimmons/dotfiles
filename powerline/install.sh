dd8adcc9bcf56aa7642ad5b6295a0863c89db517
Ryan Timmons <ryan@rytim.com>
2018-01-21 17:26:22 -0500

    git pull powerlevel9k if already exists

--- powerline/install.sh
+++ powerline/install.sh
@@ -3,7 +3,10 @@
 cd "$(dirname "$0")"
 
 if [ ! -d "powerlevel9k" ]; then
-    git clone https://github.com/bhilburn/powerlevel9k.git
+    git clone https://github.com/bhilburn/powerlevel9k.git powerlevel9k
+else
+    pushd powerlevel9k
+    git pull
 fi
 
 pip install powerline-status
