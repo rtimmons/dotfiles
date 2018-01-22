0b0ba20505698092e6c0e7e953bb2cb30b1ef803
Ryan Timmons <ryan@rytim.com>
2018-01-21 21:43:37 -0500

    highlight md files

--- less/install.sh
+++ less/install.sh
@@ -17,6 +17,7 @@ case "\$1" in
     # add all extensions you want to handle here
     *.awk|*.groff|*.java|*.js|*.m4|*.php|*.pl|*.pm|*.pod|*.sh|\\
     *.ad[asb]|*.asm|*.inc|*.[ch]|*.[ch]pp|*.[ch]xx|*.cc|*.hh|\\
+    *.md|\\
     *.lsp|*.l|*.pas|*.p|*.xml|*.xps|*.xsl|*.axp|*.ppd|*.pov|\\
     *.diff|*.patch|*.py|*.rb|*.sql|*.ebuild|*.eclass)
         $ZSH/less/venv/bin/pygmentize -O 'style=trac' "\$1" ;;
