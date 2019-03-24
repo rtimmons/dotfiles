79bd4093a9cf8e430a686868959f57494b851bea
Ryan Timmons <ryan.timmons@10gen.com>
2019-03-24 14:16:43 -0400

    fix deps for installing via pyenv

--- 050-pyenv/install.sh
+++ 050-pyenv/install.sh
@@ -4,7 +4,19 @@ easy_install pip
 pip install --upgrade pip
 
 brew install pyenv
+
+# dependencies for installing later versions of python through pyenv
 brew install readline
+brew install zlib
+brew install sqlite
+
+# https://github.com/jiansoung/issues-list/issues/13
+export LDFLAGS="${LDFLAGS} -L/usr/local/opt/zlib/lib"
+export CPPFLAGS="${CPPFLAGS} -I/usr/local/opt/zlib/include"
+export LDFLAGS="${LDFLAGS} -L/usr/local/opt/sqlite/lib"
+export CPPFLAGS="${CPPFLAGS} -I/usr/local/opt/sqlite/include"
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH} /usr/local/opt/zlib/lib/pkgconfig"
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH} /usr/local/opt/sqlite/lib/pkgconfig"
 
 # -s means skip if already done - make the thing idempotent
 pyenv install -s
