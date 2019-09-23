827c095f38ce9ae987b510665e66f5c35b253cf0
Ryan Timmons <ryan.timmons@10gen.com>
2019-09-23 10:49:48 -0400

    bring back "now + 3.days" to irb

--- ruby/install.sh
+++ ruby/install.sh
@@ -6,3 +6,12 @@ brew install ruby-build
 eval "$(rbenv init -)"
 
 rbenv install -s
+
+rbenv rehash
+
+gem install map_by_method
+gem install what_methods
+gem install pp
+gem install awesome_print
+gem install activesupport
+gem install business_time
