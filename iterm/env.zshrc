2807a74c80241a9a8438ec1efbb3fd033f82e7ff
Ryan Timmons <ryan.timmons@10gen.com>
2019-11-07 13:07:30 -0500

    install iterm2 integration

---
+++ iterm/env.zshrc
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
+    source ~/.iterm2_shell_integration.zsh
+fi
+
--- iterm/install.sh
+++ iterm/install.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/Projects/dotfiles/iterm/Preferences"
+
+if [ ! -f ~/.iterm2_shell_integration.zsh ]; then
+    curl -L https://iterm2.com/shell_integration/zsh \
+        -o ~/.iterm2_shell_integration.zsh
+fi
