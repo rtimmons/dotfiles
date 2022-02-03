4375f6cba4ea2f949b6fcc4293264838e90c3d27
Ryan Timmons <ryan.timmons@10gen.com>
2022-02-03 08:28:32 -0500

    going in further on prelue

--- emacs/install.sh
+++ emacs/install.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
-brew install emacs
-brew services start emacs
+brew install --cask emacs
+
+if [ ! -e "$ZSH/emacs/prelude" ]; then
+    git clone git://github.com/bbatsov/prelude.git "$ZSH/emacs/prelude"
+fi
+rm -f "$HOME/.emacs.d"
+ln -s "$ZSH/emacs/prelude" "$HOME/.emacs.d"
+
+pushd "$ZSH/emacs/prelude"
+    rm -rf personal
+    git pull
+    ln -s "$ZSH/emacs/prelude-personal" personal
+popd
---
+++ emacs/prelude-personal/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(yaml-mode web-mode lsp-ui lsp-mode json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
