92d24c40afd9ace95090471a39b8d65c471f02a7
Ryan Timmons <ryan.timmons@mongodb.com>
2023-08-09 11:55:26 -0400

    add qalc tool, kinda neat

---
+++ qalculate-gtk/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+brew install qalculate-gtk
+# provides: qalculate-gtk
+# provides: qalc
+# Manual https://qalculate.github.io/manual/qalculate-definitions-functions.html
+
