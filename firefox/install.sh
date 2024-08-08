c9f0707d4435fa224bf4cb4ae19b6d05b0d6afc0
Ryan Timmons <ryan.timmons@mongodb.com>
2024-08-08 13:27:26 -0400

    firefox: hide bookmark bar icons

--- firefox/install.sh
+++ firefox/install.sh
@@ -4,3 +4,38 @@ set -eou pipefail
 
 brew install firefoxpwa
 
+# https://support.mozilla.org/en-US/kb/profiles-where-firefox-stores-user-data
+# https://www.userchrome.org/how-create-userchrome-css.html
+
+
+mydir=$(dirname "$0")
+pushd "${mydir}" >/dev/null
+    mydir=$(pwd -P)
+popd > /dev/null
+
+chrome_dir="${mydir}/chrome"
+if [ ! -d "${chrome_dir}" ]; then
+    echo "No chrome dir ${chrome_dir}" >/dev/null
+    exit 10
+fi
+
+dest="${HOME}/Library/Application Support/Firefox/Profiles"
+if [ ! -d "${dest}" ]; then
+    echo "No Firefox Profiles in $dest"
+    exit 0
+fi
+
+for D in "${dest}"/*; do
+    echo "Processing ${D}"
+    # pushd "${D}" >/dev/null
+    if [ -e "${D}/chrome" ]; then
+        if [ ! -L "${D}/chrome" ]; then
+            echo "${D}/chrome exists and isn't a symlink" >/dev/null
+            exit 1
+        fi
+    else
+        ln -s "${chrome_dir}" "${D}/chrome"
+    fi
+    # popd >/dev/null
+done
+
