31ad36e8fdf43bb2bb1c8414401fa8f198f74b47
Ryan Timmons <rtimmons@android-acb62ef6e31e6e34.localdomain>
2024-11-21 18:33:59 -0500

    fix iterm preferences loading

--- iterm/install.sh
+++ iterm/install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+# https://shyr.io/blog/sync-iterm2-configs
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/Projects/dotfiles/iterm/Preferences"
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
 
 if [ ! -f ~/.iterm2_shell_integration.zsh ]; then
     curl -L https://iterm2.com/shell_integration/zsh \
