1f46e83e6df3b766f1e41a04f0da131c47be41ef
Ryan Timmons <ryan.timmons@mongodb.com>
2025-06-18 10:53:45 -0400

    fix claude installation

--- claude/install.sh
+++ claude/install.sh
@@ -2,6 +2,10 @@
 set -eou pipefail
 
 cd "$(dirname "$0")" || exit 1
+
+source "$(brew --prefix nvm)/libexec/nvm.sh"
+nvm install
+
 "$(brew --prefix nvm)/nvm-exec" npm install -g @anthropic-ai/claude-code
 
 command -v claude
